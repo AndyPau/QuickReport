454947d824a55e18396d83f814ce11d609705633
andypau <andy.pau@qq.com>
2016-10-10 16:42:31 +0800

    support global function to access node_modules

--- manager.py
+++ manager.py
@@ -1,21 +1,22 @@
 # coding = utf-8
 
-from flask import Flask
-from flask_bootstrap import Bootstrap
+from flask import Flask, send_from_directory
 from flask import render_template
 from flask_sslify import SSLify
-import  requests
 
 app = Flask(__name__, template_folder='static', static_folder='static')
-bootstrap = Bootstrap(app)
 sslify = SSLify(app)
 
-# app.jinja_env.variable_start_string = '{{ '
-# app.jinja_env.variable_end_string = ' }}'
+
+# 默认情况下，浏览器无法加载根目录下node_modules内的资源文件，增加该方法来处理请求
+# TODO:需要深入研究一下其他处理办法
+@app.route('/node_modules/<path:path>')
+def send_npm_assets(path):
+    return send_from_directory('node_modules', path)
 
 
 @app.route('/')
-def dailyreport_dashboard():
+def index():
     return render_template('index.html')
 
 
