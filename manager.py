a832c95d4f252737bfcaa5ed9eacf274821fe0d6
andypau <andy.pau@qq.com>
2016-10-19 22:41:51 +0800

    clean manager.py code

--- manager.py
+++ manager.py
@@ -1,49 +1,20 @@
 # coding = utf-8
 
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, jsonify
 from flask import render_template
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
-from flask_sslify import SSLify
 import template.charts.line_option
 
 app = Flask(__name__, template_folder='static', static_folder='static')
 app.config.from_object('config')
-api = Api(app)
 database = SQLAlchemy(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('admin', type=bool, help='Use super manager mode', default=False)
 
-sslify = SSLify(app)
-
-resource_fields = {
-    'id': fields.Integer,
-    'name': fields.String,
-    'address': fields.String
-}
-
-
-class AngularResource(Resource):
-    # @marshal_with(resource_fields)
-    def get(self, name=None):
-        if name is not None:
-            return {'message': name}
-        else:
-            return {'message': 'all names you want'}
-
-            # user = User.query.filter_by(name=name).first()
-            # return user
-
-
-api.add_resource(AngularResource, '/api/v1/user/<name>', '/api/v1/user/')
-
-
-# 默认情况下，浏览器无法加载根目录下node_modules内的资源文件，增加该方法来处理请求
-# TODO:需要深入研究一下其他处理办法
-@app.route('/node_modules/<path:path>')
-def send_npm_assets(path):
-    return send_from_directory('node_modules', path)
+# # 默认情况下，浏览器无法加载根目录下node_modules内的资源文件，增加该方法来处理请求
+# # TODO:需要深入研究一下其他处理办法
+# @app.route('/node_modules/<path:path>')
+# def send_npm_assets(path):
+#     return send_from_directory('node_modules', path)
 
 
 @app.route('/')
@@ -52,12 +23,12 @@ def index():
 
 
 @app.route('/api/v1/echartcase/<date>')
-def echartcase(date):
+def facebook(date):
     return jsonify(option=template.line_template_option)
 
 
 @app.route('/api/v1/tablecase/<date>')
-def tablecase(date):
+def daily_report_traffic(date):
     data = [
         {
             "id": 0,
--- static/js/angular1.x/module.js
+++ static/js/angular1.x/module.js
@@ -126,6 +126,9 @@ QuickReportAppModule.controller('MainTableCtrl', function ($scope, $http, myShar
                     url: '/api/v1/tablecase/' + $scope.dtInput,
                     method: 'GET',
                     detailView: false,
+                    showExport: true,
+                    exportDataType: 'all',
+                    exportTypes: ['png'],
                     rowStyle: function (row, index) {
                         var classes = ['active', 'success', 'info', 'warning', 'danger'];
                         if (index % 2 === 0 && index / 2 < classes.length) {
